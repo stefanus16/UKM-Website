ee550fc072dd8e818225761db2448cbd01175a0f
stefanus16 <117900966+stefanus16@users.noreply.github.com>
2025-03-09 12:54:17 +0700

    updt db

--- dbukm_new.sql
+++ dbukm_new.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 18, 2025 at 07:37 AM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- Waktu pembuatan: 09 Mar 2025 pada 06.52
+-- Versi server: 10.4.32-MariaDB
+-- Versi PHP: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,13 +18,30 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `dbukm`
+-- Database: `dbukm_new`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `kehadiran`
+-- Struktur dari tabel `admin`
+--
+
+CREATE TABLE `admin` (
+  `password` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `admin`
+--
+
+INSERT INTO `admin` (`password`) VALUES
+('ukm@2025badmin');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `kehadiran`
 --
 
 CREATE TABLE `kehadiran` (
@@ -34,10 +51,27 @@ CREATE TABLE `kehadiran` (
   `jumlahhadir` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data untuk tabel `kehadiran`
+--
+
+INSERT INTO `kehadiran` (`idhadir`, `idpeserta`, `tanggalhadir`, `jumlahhadir`) VALUES
+(1, 1, '2025-02-18 14:43:30', 1),
+(2, 1, '2025-02-18 14:45:19', 1),
+(3, 1, '2025-02-18 14:49:33', 1),
+(4, 2, '2025-03-09 06:03:23', 1),
+(5, 3, '2025-03-09 06:05:18', 1),
+(6, 1, '2025-03-09 06:09:51', 1),
+(7, 4, '2025-03-09 06:24:34', 1),
+(8, 4, '2025-03-09 06:24:55', 1),
+(9, 1, '2025-03-09 06:44:37', 1),
+(10, 3, '2025-03-09 06:48:01', 1),
+(11, 5, '2025-03-09 06:48:27', 1);
+
 -- --------------------------------------------------------
 
 --
--- Table structure for table `peserta`
+-- Struktur dari tabel `peserta`
 --
 
 CREATE TABLE `peserta` (
@@ -46,11 +80,22 @@ CREATE TABLE `peserta` (
   `nama` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data untuk tabel `peserta`
+--
+
+INSERT INTO `peserta` (`idpeserta`, `nim`, `nama`) VALUES
+(1, 71220831, 'steven'),
+(2, 61230892, 'agus'),
+(3, 71220999, 'james'),
+(4, 41230000, 'babi'),
+(5, 71263972, 'rudi');
+
 -- --------------------------------------------------------
 
 --
--- Stand-in structure for view `peserta_kehadiran`
--- (See below for the actual view)
+-- Stand-in struktur untuk tampilan `peserta_kehadiran`
+-- (Lihat di bawah untuk tampilan aktual)
 --
 CREATE TABLE `peserta_kehadiran` (
 `idpeserta` int(11)
@@ -62,7 +107,7 @@ CREATE TABLE `peserta_kehadiran` (
 -- --------------------------------------------------------
 
 --
--- Structure for view `peserta_kehadiran`
+-- Struktur untuk view `peserta_kehadiran`
 --
 DROP TABLE IF EXISTS `peserta_kehadiran`;
 
@@ -73,40 +118,40 @@ CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW 
 --
 
 --
--- Indexes for table `kehadiran`
+-- Indeks untuk tabel `kehadiran`
 --
 ALTER TABLE `kehadiran`
   ADD PRIMARY KEY (`idhadir`),
   ADD KEY `idpeserta` (`idpeserta`);
 
 --
--- Indexes for table `peserta`
+-- Indeks untuk tabel `peserta`
 --
 ALTER TABLE `peserta`
   ADD PRIMARY KEY (`idpeserta`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT untuk tabel yang dibuang
 --
 
 --
--- AUTO_INCREMENT for table `kehadiran`
+-- AUTO_INCREMENT untuk tabel `kehadiran`
 --
 ALTER TABLE `kehadiran`
-  MODIFY `idhadir` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `idhadir` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
--- AUTO_INCREMENT for table `peserta`
+-- AUTO_INCREMENT untuk tabel `peserta`
 --
 ALTER TABLE `peserta`
-  MODIFY `idpeserta` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `idpeserta` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
--- Constraints for dumped tables
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
 --
 
 --
--- Constraints for table `kehadiran`
+-- Ketidakleluasaan untuk tabel `kehadiran`
 --
 ALTER TABLE `kehadiran`
   ADD CONSTRAINT `kehadiran_ibfk_1` FOREIGN KEY (`idpeserta`) REFERENCES `peserta` (`idpeserta`);
